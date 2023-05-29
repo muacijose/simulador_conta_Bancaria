9565b3969fe64a1385238a642208db3d0bb21ada
muaci <muaci@outlook.com>
2023-05-29 19:37:20 -0300

    inicio projeto

---
+++ src/ContaTerminal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o seu nome");
+        String nome = sc.next();
+        System.out.println("Por favor, Digite o número da Agência.");
+        String agencia = sc.next();
+        System.out.println("Por favor, Digite o  nùmero da sua Conta.");
+        int numero = sc.nextInt();
+        System.out.println();
+
+
+
+    }
+}
